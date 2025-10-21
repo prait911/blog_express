45f140b7101f8d8f40fe302c029924f7a4294a94
prait <prait.kirilo@voco.ee>
2025-10-21 09:16:00 +0300

    added routes folder

--- index.js
+++ index.js
@@ -3,21 +3,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { getPosts, createPost, getPostbyId, updatePost, publishPost, deletePost } = require('./controllers/posts');
-
-app.get('/posts', (req, res) => getPosts(req, res));
-
-app.post('/posts', (req, res) => createPost(req, res));
-
-app.get('/posts/:id', (req, res) => getPostbyId(req, res));
-
-app.put('/posts/:id', (req, res) => updatePost(req, res));
-
-app.patch('/posts/:id/publish', (req, res) => publishPost(req, res));
-
-app.delete('/posts/:id', (req, res) => deletePost(req, res));
+const postsRoutes = require('./routes/posts.js')
+app.use('/posts', postsRoutes)
 
 app.listen(3002, () => {
-  console.log("Server running at http://localhos:3002/")
+  console.log("Server is running at http://localhos:3002/")
 });
 
